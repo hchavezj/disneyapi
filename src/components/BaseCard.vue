34bb0bc950a607a2453ae24ade5a9256673a3350
hchavezj <hchavezj@gmail.com>
2022-10-09 00:04:40 -0500

    Update API, changed names, added router

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,20 +1,27 @@
 <script setup>
-    const props = defineProps({
-        character: {
-            type: Object,
-            default: () => ({
-                _id: -1,
-                name: 'Unknown',
-                imageUrl: 'https://via.placeholder.com/150',
-            }),
-        },
-    })
+const props = defineProps({
+  character: {
+    type: Object,
+    default: () => ({
+      _id: -1,
+      name: 'Unknown',
+      imageUrl: 'https://via.placeholder.com/150',
+    }),
+  },
+})
 </script>
 
 <template>
-    <div :to="`/character/${props.character._id}`"
-    class="rounded-lg bg-slate-900 shadow-lg">
-        <img class="h-3/4 w-full rounded-t-lg object-cover object-left-top" :src="props.character.imageUrl" alt=""/>
-        <h2>{{ props.character.name }}</h2>
-    </div>
+  <RouterLink
+    :to="`/character/${props.character._id}`"
+    class="rounded-lg bg-slate-900 shadow-lg"
+  >
+    <img
+      class="h-3/4 w-full rounded-t-lg object-cover object-top"
+      :src="props.character.imageUrl"
+    />
+    <h2 class="text-center text-2xl font-semibold text-gray-400">
+      {{ props.character.name }}
+    </h2>
+  </RouterLink>
 </template>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from 'vue';
+// import { ref } from 'vue';
+// 
+// const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>Hello</h1>
+    <card />
+  </div>
+
+</template>
+
+<script>
+  import card from '@/components/BaseCard.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      card,
+    },
+  }
+</script>
+
+<style scoped>
+</style>
